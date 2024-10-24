fa5251868c1bb0b685da6945b3f802297f0b46bd
DavlatSunnatov <153508965+DavlatSunnativ@users.noreply.github.com>
2024-10-24 17:15:59 +0600

    Update Main.java

--- TaskOne/src/Main.java
+++ TaskOne/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         try {
-            String expression = "3 4 + 2 -"; // Example of a valid expression
+            String expression = "3 4 + 2 -"; 
             int result = Interpreter.evaluate(expression);
             System.out.println("Result: " + result);
         } catch (IllegalArgumentException e) {
